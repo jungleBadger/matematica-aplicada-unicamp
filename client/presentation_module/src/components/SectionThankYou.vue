4bc68d1af951484d86d22db6c902ef5c9b4f7cf0
Daniel Abrao <danielpaladar@gmail.com>
2025-08-15 11:22:14 -0300

    Adjust image path

--- client/presentation_module/src/components/SectionThankYou.vue
+++ client/presentation_module/src/components/SectionThankYou.vue
@@ -2,11 +2,11 @@
 
   <section>
     <section
-        data-background-video="static/flame-bg-frame.mp4">
+        data-background-video="semana-matematica-aplicada/static/flame-bg-frame.mp4">
       Thank you
     </section>
 
-    <section data-background-size="contain" data-background-image="static/pic6.jpg"></section>
+    <section data-background-size="contain" data-background-image="semana-matematica-aplicada/static/pic6.jpg"></section>
 
     <section>
       And remember...
